356c924924c39b8b91f4e53b80a24ed22b58e019
nanda ricco <nandaricco2677@gmail.com>
2024-09-06 14:39:56 +0700

    halo

---
+++ PerkenalanDiri.java
@@ -0,0 +1,9 @@
+public class PerkenalanDiri {
+
+    public static void main(String[] args) {
+        System.out.println("Nama   : Nanda Ricco Satria Indrawan");
+        System.out.println("Nim    : 244107060058");
+        System.out.println("Alamat : Jl Sawojajar Gg 19 no 49H");
+
+    }
+}
