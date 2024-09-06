4467ac9aee006b8d9f0f8acfb8cb64554f61fcf7
NandaRicco <88094296+NandaRicco@users.noreply.github.com>
2024-09-06 14:09:09 +0700

    Update PerkenalanDiri14.java

--- PerkenalanDiri14.java
+++ PerkenalanDiri14.java
@@ -3,7 +3,7 @@ public class PerkenalanDiri14 {
     public static void main(String[] args) {
         System.out.println("Nama   : Nanda Ricco Satria Indrawan");
         System.out.println("Nim    : 244107060058");
-        System.out.println("Alamat : Jl Sawojajar Gg 19 no49H");
+        System.out.println("Alamat : Jl Sawojajar Gg 19 no49");
 
     }
 }
