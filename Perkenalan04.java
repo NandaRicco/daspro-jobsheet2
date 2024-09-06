be521ffb5fa6fb1c57a39c8e8477bcd306974b7b
F4dhil54 <fadilsyahidan14@gmail.com>
2024-09-06 15:07:20 +0700

    pp

---
+++ Perkenalan04.java
@@ -0,0 +1,8 @@
+public class Perkenalan04{
+
+    public static void main (String[] args){
+        System.out.println("Nama   : Fadhil Syahidan Arizki");
+        System.out.println("Nim   : 244107060125");
+        System.out.println("Alamat : JL.Ikan Kakap IX/2");
+    }
+}
